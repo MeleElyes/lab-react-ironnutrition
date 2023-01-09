13782aa69bf164cd12a57f3729d1d26ed68cc92d
Elyes MELE <mele.elyes@gmail.com>
2023-01-09 17:32:57 +0100

    lab done

--- src/Component/AddFoodForm.js
+++ src/Component/AddFoodForm.js
@@ -1,42 +1,65 @@
 import { Divider, Input } from 'antd';
-
+import { useState } from 'react';
 
 const AddFoodForm = (props) => {
-
-const addFood = (event) => {
+  const [newFood, setNewFood] = useState({
+    name: '',
+    calories: '',
+    image: '',
+    servings: '',
+  });
+  const addFood = (event) => {
     event.preventDefault();
-let arrayInput = document.querySelectorAll("input")
-let newObjectToAdd = {
-    "name": arrayInput[0].value,
-    "calories": arrayInput[1].value,
-    "image": arrayInput[2].value,
-    "servings": arrayInput[3].value
-  }
-props.setFood([newObjectToAdd,...props.food])
-}
-   
+    props.setFood([newFood, ...props.food]);
+  };
+
+  const addNewFood = (e) => {
+    setNewFood((newFood) => ({ ...newFood, [e.target.name]: e.target.value }));
+  };
+
+  console.log(newFood)
   return (
     <form>
       <Divider>Add Food Entry</Divider>
 
       <label>Name</label>
-      <Input value={undefined} type="text"  />
+      <Input
+        value={undefined}
+        type="text"
+        name="name"
+        onChange={(e) => addNewFood(e)}
+      />
 
       <label>Image</label>
-      
-      <Input value={undefined} type="text"  />
+
+      <Input
+        value={undefined}
+        type="text"
+        name="image"
+        onChange={(e) => addNewFood(e)}
+      />
 
       <label>Calories</label>
-      <Input value={undefined} type="number"  />
+      <Input
+        value={undefined}
+        type="number"
+        name="calories"
+        onChange={(e) => addNewFood(e)}
+      />
 
       <label>Servings</label>
-      <Input value={undefined} type="number"  />
+      <Input
+        value={undefined}
+        type="number"
+        name="servings"
+        onChange={(e) => addNewFood(e)}
+      />
 
-      <button type="submit" onClick={(event)=> addFood(event)}>Create</button>
+      <button type="submit" onClick={(event) => addFood(event)}>
+        Create
+      </button>
     </form>
-
-
   );
-}
+};
 
 export default AddFoodForm;
